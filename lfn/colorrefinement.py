5955947686c1e2f82f9dc45da233f3890d17f955
Jens2 <j.vdmeer10@hotmail.com>
2016-02-22 14:40:04 +0100

    .

--- lfn/colorrefinement.py
+++ lfn/colorrefinement.py
@@ -3,13 +3,16 @@ from graphImplementations.fastgraphs import graph
 def colorref(G):
     listofcolours = []
     #
-    # create a list which contains the colour number as the index with a list of vertices with the specified colour.
-    #
+    # Create a list which contains the colour number as the index with a list of vertices which all have the specified colour.
+    # Also keep track of the highest known number
+    highestDeg = - 1
     for v in G.V():
-        if listofcolours[v.getDegree] is not None:
-            listofcolours.insert(v.getDegree, listofcolours[v.getDegree].append(v))
+        if highestDeg < v.getColornum:
+            highestDeg = v.getColornum
+        if listofcolours[v.getColornum] is not None:
+            listofcolours.insert(v.getColornum, listofcolours[v.getColornum].append(v))
         else:
-            listofcolours.insert(v.getDegree, list().append(v))
+            listofcolours.insert(v.getColornum, list().append(v))
     i = 0
     # while not done
 
