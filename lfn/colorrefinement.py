f5a09f31b8b1e426d51846805395c57c3d565633
GijsBeernink <g.j.beernink@student.utwente.nl>
2016-02-22 13:32:55 +0100

    checkNeighbourhood

--- lfn/colorrefinement.py
+++ lfn/colorrefinement.py
@@ -1,6 +1,18 @@
 from graphImplementations.fastgraphs import graph
+import graphImplementations.basicgraphs
+
 def colorref(G):
     a = dict()
     for v in G.V():
         a[v] = 1
     i = 0
+
+def checkNeighbourhood(u, v):
+    if len(u.nbs()) == len(v.nbs()):
+        for n in u.nbs():
+            if n not in v.nbs():
+                return False
+        return True
+    else:
+        return False
+
