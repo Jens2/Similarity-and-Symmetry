25741e4c9ad1794b77f105d03bd2681b077ab6d8
GijsBeernink <g.j.beernink@student.utwente.nl>
2016-02-22 14:50:27 +0100

    deleted unused functions

--- lfn/colorrefinement.py
+++ lfn/colorrefinement.py
@@ -14,18 +14,3 @@ def colorref(G):
     # while not done.
 
     return -1
-
-
-
-# Checks for equality of neighbours
-def checkNeighbourhood(u, v):
-    if len(u.nbs()) == len(v.nbs()):
-        for n in u.nbs():
-            if n not in v.nbs():
-                return False
-        return True
-    else:
-        return False
-
-def checkDegree(u, v):
-    return len(u.getDegree()) == len(v.getDegree())
