f81683430bea0b779f56fc660f405f8602e84d85
GijsBeernink <g.j.beernink@student.utwente.nl>
2016-02-24 15:14:43 +0100

    isBalanced done

--- lfn/individualizationrefinement.py
+++ lfn/individualizationrefinement.py
@@ -17,14 +17,11 @@ def individualizationref(dict):
                 D.append(element)
             else:
                 I.append(element)
-    countIsomorphism(D, I)
-
-
-dict = dict({2: [0, 6, 8, 9], 3: [1, 5, 10, 13], 4: [2, 4, 7, 12], 5: [3, 11]})
-individualizationref(dict)
+    # countIsomorphism(D, I)
+#
 
 def countIsomorphism(D, I):
-    if not isBalanced(D, I):
+    if not isBalanced(dict):
         return 0
     if isBijection(D, I):
         return 1
@@ -32,9 +29,26 @@ def countIsomorphism(D, I):
 
     # TODO implement
 
-def isBalanced(D, I):
+def isBalanced(dict):
+    arrays = dict.values()
+    result = []
+    for array in arrays:
+        for element in array:
+            result.append(element)
 
-    pass # TODO implement
+    length = len(result)
+    counter = 0
+    for key in dict.keys():
+        for element in dict.get(key):
+            if element < length/2:
+                counter+=1
+            else:
+                counter-=1
+    return counter == 0
+
+dict = dict({2: [0, 6, 8, 9], 3: [1, 5, 10, 13], 4: [2, 4, 7, 12], 5: [3, 11]})
+print(isBalanced(dict))
+
 
 def isBijection(D, I):
 
