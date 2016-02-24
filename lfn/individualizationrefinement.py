d38dd1290690a071e298df26c47ffad0ac2df608
GijsBeernink <g.j.beernink@student.utwente.nl>
2016-02-24 14:11:15 +0100

    merge

---
+++ lfn/individualizationrefinement.py
@@ -0,0 +1,15 @@
+from lfn.colorrefinement import *
+from util import *
+
+
+def countIsomorphism(D, I):
+
+    pass # TODO implement
+
+def isBalanced(D, I):
+
+    pass # TODO implement
+
+def isBijection(coloring):
+
+    pass # TODO implement
