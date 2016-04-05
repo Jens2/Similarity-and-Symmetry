2c697ca097517352f6aafe6f5778323b4472845e
GijsBeernink <g.j.beernink@student.utwente.nl>
2016-04-05 23:09:38 +0200

    Printen van antwoorden

--- lfn/printResults.py
+++ lfn/printResults.py
@@ -1,7 +1,10 @@
 def printIsomorphicGraphs(ListsInList):
     print("Sets of isomorphic graphs:")
-    for i in range(0, len(list)):
-        print("[" + str(list[i][0]) + ", " + str(list[i][1]) + "]")
+    if list[0][0] == None:
+        print("No isomorphic graphs")
+    else:
+        for i in range(0, len(list)):
+            print("[" + str(list[i][0]) + ", " + str(list[i][1]) + "]")
 
 def printNumberAut(listsInList):
     print("Graph:   Number of automorphisms:")
