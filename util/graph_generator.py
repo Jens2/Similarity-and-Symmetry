ae92870ed21ac4a369849e590ad4fc1a99d25a7e
ruben <ruben4@live.nl>
2016-04-05 17:23:31 +0200

    trees

--- util/graph_generator.py
+++ util/graph_generator.py
@@ -64,3 +64,39 @@ def create_planar(n):
 #
 # print(create_bipartite_complete(4, 4))
 # print(create_complete(4))
+
+def create_tree(n):
+    g = graph(n)
+    V = g.V()
+    E = g.E()
+    G1 = []
+    G2 = []
+    G3 = []
+    root = random.choice(list(V))
+    print(root)
+    split1 = random.choice(list(range(1,n-1)))
+    split2 = random.choice(list(range(split1+1,n)))
+    if V[split1] == root:
+        split1 = split1 + 1
+    if V[split2] == root or V[split2] == V[split1]:
+        split2 = split2 - 1
+    print(split1)
+    print(split2)
+    for i in range(0,split1):
+        if V[i] != root:
+            G1.append(V[i])
+    for i in range(split1,split2):
+        if V[i] != root:
+            G2.append(V[i])
+    for i in range(split2,n):
+        if V[i] != root:
+            G3.append(V[i])
+    g.addedge(root,random.choice(G1))
+    g.addedge(root,random.choice(G2))
+    g.addedge(root,random.choice(G3))
+    print(G1)
+    print(G2)
+    print(G3)
+    return g
+
+print(create_tree(9))
